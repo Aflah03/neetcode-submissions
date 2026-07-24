76e2f014142f4cf39296354b32449c95f3eca921
AFLAH MUHAMMED P <140427031+Aflah03@users.noreply.github.com>
2026-07-24 20:09:24 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self):
+        self.terminal = False
+        self.arr  = [None] * 26
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        temp = self.root
+        for c in word:
+            idx = ord(c)- ord('a')
+            if(temp.arr[idx] is None):
+                temp.arr[idx] = Node()
+            temp = temp.arr[idx]
+        temp.terminal = True
+
+
+    def search(self, word: str) -> bool:
+        temp = self.root
+        for c in word:
+            idx = ord(c) - ord('a')
+            if(temp.arr[idx] is None):
+                return False
+            temp = temp.arr[idx]
+        return temp.terminal
+
+    def startsWith(self, prefix: str) -> bool:
+        temp = self.root 
+        for i in range(len(prefix)):
+            idx = ord(prefix[i])- ord('a')
+            if(temp.arr[idx] is None):
+                return False
+            temp = temp.arr[idx]
+        return True
